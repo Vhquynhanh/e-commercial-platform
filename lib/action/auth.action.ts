79d5a1a9edf8bd2338f382e0ad7f8da1f5b82bb7
Vhquynhanh <vhqa2004@gmail.com>
2025-07-14 12:08:58 +0700

    Fix for building deploy

--- lib/action/auth.action.ts
+++ lib/action/auth.action.ts
@@ -1,13 +1,11 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import { signOut } from "@/auth"; // Xóa phần này nếu bạn không cần signOut nữa
 
 export const signInAction = async (loginInfo: string, password: string) => {
   return true;
 };
 
 export const signOutAction = async () => {
-  await signOut();
   redirect("/sign-in");
 };
